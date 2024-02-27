74b62864c9919b7561e0d6823a66994c8a972f1a
Steven Suddreth <stevensuddreth@gmail.com>
2024-02-27 17:41:42 -0500

    implement prodcut-loader component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import Accordion from './components/accordion/accordion-index';
 import ImageSlider from './components/image-slider/image-slider-index';
+import LoadMoreData from './components/load-more-data/load-more-index';
 import RandomColor from './components/random-color/random-color-index';
 import StarRating from './components/star-rating/star-rating-index';
 
 function App() {
   return (
     <div className="App">
-      {/* <Accordion />
+      <Accordion />
       <RandomColor />
-      <StarRating noOfStars={5} /> */}
+      <StarRating noOfStars={5} />
       <ImageSlider url={"https://picsum.photos/v2/list"} page={"1"} limit={"10"} />
+      <LoadMoreData />
     </div>
   );
 }
